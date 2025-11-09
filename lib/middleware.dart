d2492ddf664adab142bf636e04a54393c215a2b6
Zachareee <zacharytanhx@yahoo.com>
2025-11-09 16:29:23 +0800

    Fixed METHOD NOT ALLOWED, pin dart version

--- lib/middleware.dart
+++ lib/middleware.dart
@@ -3,9 +3,6 @@ import 'package:shelf/shelf.dart';
 Middleware rejectBadRequests() {
   return (innerhandler) {
     return (request) {
-      if (request.method != 'POST') {
-        return Response(405, body: "Method not Allowed");
-      }
       return innerhandler(request);
     };
   };
